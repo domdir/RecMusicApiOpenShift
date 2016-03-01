d0978407102fe12fdf47d692be1ee536148467ea
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 19:56:38 +0100

    test

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api
+from flask_restful import Resource
 from pymongo import MongoClient
 
 import os
@@ -7,13 +8,10 @@ import os
 app = Flask(__name__)
 api=Api(app)
 
-@app.route('/')
-def hello_world():
-    print "test"
-    #client = MongoClient("mongodb://$OPENSHIFT_MONGODB_DB_HOST:OPENSHIFT_MONGODB_DB_PORT/")
-    #db = client.recmusicapiopenshift
-
-    return 'test World!'
+class ResetServer(Resource):
+    def get(self):
+        return "test"
+api.add_resource(ResetServer, '/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
