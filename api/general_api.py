b5b2ee6838698046aef3b3bfd7bb201d372f5642
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 23:02:12 +0100

    test

---
+++ api/general_api.py
@@ -0,0 +1,19 @@
+from api import api, rec_music_server
+import json
+import os
+from api import client
+
+from flask_restful import Resource
+
+class ResetServer(Resource):
+    def get(self):
+        file_path = os.getcwd() + "/data/songs.json"
+        songsList = json.load(open(file_path))
+        db = client.rec_music_core_db
+        db.rates.remove()
+        db.songs.remove()
+        songs = db.songs
+        songs.insert_many(songsList)
+        collections_names=db.collection_names()
+        return "db created"
+api.add_resource(ResetServer, '/reset')
