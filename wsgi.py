3b1bcc940ae45a82b5c3e17579ba13b4882bacec
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 15:48:41 +0100

    test

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
