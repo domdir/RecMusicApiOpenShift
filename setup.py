13d68e2649b99d0be89b10ad7793be50877e4df1
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 16:30:21 +0100

    test

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
-setup(name='RecMusicApi',
+setup(name='YourAppName',
       version='1.0',
       description='OpenShift App',
-      author='forgiangi',
-      author_email='gianlucachiap@gmail.com',
-      url='http://www.python.org/sigs/distutils-sig/'
+      author='Your Name',
+      author_email='example@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+#      install_requires=['Django>=1.3'],
      )
