2ef72ada28f824959ade9858303092c4d7d6d5c0
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 23:51:06 +0100

    test

--- api/general_api.py
+++ api/general_api.py
@@ -12,8 +12,8 @@ api.add_resource(Home, '/')
 
 class ResetServer(Resource):
     def get(self):
-        file_path = os.getcwd() + "/data/songs.json"
-        #songsList = json.load(open(file_path))
+        file_path = os.getcwd() + "/app-deployments/current/repo/data/songs.json"
+        songsList = json.load(open(file_path))
         #db = client.rec_music_core_db
         #db.rates.remove()
         #db.songs.remove()
