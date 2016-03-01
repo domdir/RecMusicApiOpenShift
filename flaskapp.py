ec327c9b0325af83378de4b033b7f5ed5b791ad5
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 22:47:58 +0100

    test

--- flaskapp.py
+++ flaskapp.py
@@ -4,18 +4,14 @@ from flask_restful import Resource
 from pymongo import MongoClient
 
 app = Flask(__name__)
-#api=Api(app)
-#client = MongoClient('localhost', 27017)
+api=Api(app)
+client = MongoClient('localhost', 27017)
 
-#class ResetServer(Resource):
-#    def get(self):
-#        return "test1"
+class ResetServer(Resource):
+    def get(self):
+        return "test1"
 
-#api.add_resource(ResetServer, '/')
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+api.add_resource(ResetServer, '/')
 
 if __name__ == '__main__':
     app.run()
