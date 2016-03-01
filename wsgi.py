8aa281dbbe353ab4adf7ad636629c410b275c57b
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 16:29:04 +0100

    test

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
-virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
@@ -21,4 +21,4 @@ if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
-    httpd.serve_forever()
+    httpd.handle_request()
