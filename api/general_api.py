ca56bf21c841b62c5633a48ec4acbb80c0647f66
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 23:39:47 +0100

    test

--- api/general_api.py
+++ api/general_api.py
@@ -12,13 +12,13 @@ api.add_resource(Home, '/')
 
 class ResetServer(Resource):
     def get(self):
-        file_path = os.getcwd() + "/data/songs.json"
-        songsList = json.load(open(file_path))
-        db = client.rec_music_core_db
-        db.rates.remove()
-        db.songs.remove()
-        songs = db.songs
-        songs.insert_many(songsList)
-        collections_names=db.collection_names()
+        #file_path = os.getcwd() + "/data/songs.json"
+        #songsList = json.load(open(file_path))
+        #db = client.rec_music_core_db
+        #db.rates.remove()
+        #db.songs.remove()
+        #songs = db.songs
+        #songs.insert_many(songsList)
+        #collections_names=db.collection_names()
         return "db created"
 api.add_resource(ResetServer, '/reset')
