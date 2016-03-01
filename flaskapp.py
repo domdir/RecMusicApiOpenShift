5dc93582e6dc33936e276da6c25baeb17fc7121b
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 17:56:01 +0100

    test

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_restful import Api
+import pymongo
+import os
+
 app = Flask(__name__)
+api=Api(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    mongo_con = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_HOST'],
+                               int(os.environ['OPENSHIFT_MONGODB_DB_PORT']))
+    db = mongo_con[os.environ['OPENSHIFT_APP_NAME']]
+    return 'test World!'
 
 if __name__ == '__main__':
     app.run()
