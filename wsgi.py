29af5b9563f7facebb943dd04f0f6c7da227ef37
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 16:32:08 +0100

    test

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 import os
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
@@ -13,7 +11,26 @@ except IOError:
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-from flaskapp import app as application
+
+def application(environ, start_response):
+
+    ctype = 'text/plain'
+    if environ['PATH_INFO'] == '/health':
+        response_body = "1"
+    elif environ['PATH_INFO'] == '/env':
+        response_body = ['%s: %s' % (key, value)
+                    for key, value in sorted(environ.items())]
+        response_body = '\n'.join(response_body)
+    else:
+        ctype = 'text/html'
+        response_body = 'ok'
+
+    status = '200 OK'
+    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    #
+    start_response(status, response_headers)
+    return [response_body]
+
 #
 # Below for testing only
 #
