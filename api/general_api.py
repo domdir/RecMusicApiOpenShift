87bfc0507cfda2306a3e384ec2c36708e00ef867
gianluca chiap <gianlucachiap@gmail.com>
2016-03-01 23:52:32 +0100

    test

--- api/general_api.py
+++ api/general_api.py
@@ -14,11 +14,11 @@ class ResetServer(Resource):
     def get(self):
         file_path = os.getcwd() + "/app-deployments/current/repo/data/songs.json"
         songsList = json.load(open(file_path))
-        #db = client.rec_music_core_db
+        db = client.recmusicapiopenshift
         #db.rates.remove()
         #db.songs.remove()
-        #songs = db.songs
-        #songs.insert_many(songsList)
+        songs = db.songs
+        songs.insert_many(songsList)
         #collections_names=db.collection_names()
-        return file_path
+        return "db created"
 api.add_resource(ResetServer, '/reset')
