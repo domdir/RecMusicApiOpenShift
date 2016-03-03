915df7ce18a569c5527e8c603d7172b8f5d99d94
gianluca chiap <gianlucachiap@gmail.com>
2016-03-03 20:22:37 +0100

    test

--- api/general_api.py
+++ api/general_api.py
@@ -12,7 +12,7 @@ api.add_resource(Home, '/')
 
 class ResetServer(Resource):
     def get(self):
-        client = MongoClient('localhost', 27017)
+        client = MongoClient("127.5.34.130", 27017)
         #file_path = os.getcwd() + "/app-deployments/current/repo/data/songs.json"
         #songsList = json.load(open(file_path))
         db = client.recmusicapiopenshift
