e5d343f2460c281b4207b7206a2021330508cf8b
gianluca chiap <gianlucachiap@gmail.com>
2016-03-03 21:20:31 +0100

    test

--- api/general_api.py
+++ api/general_api.py
@@ -1,33 +1,22 @@
-from flaskapp import api
+from flaskapp import api,db
 import json
 import os
 from flask_restful import Resource
-from pymongo import MongoClient
+
 
 class Home(Resource):
     def get(self):
         return "home"
-
 api.add_resource(Home, '/')
 
 class ResetServer(Resource):
     def get(self):
-        client = MongoClient("127.5.34.130", 27017)
-        db = client.recmusicapiopenshift
-        db.authenticate("admin","FeLZebfCiq_x")
-
         file_path = os.getcwd() + "/app-deployments/current/repo/data/songs.json"
         songsList = json.load(open(file_path))
         db.songs.remove()
+        db.rates.remove()
+        db.test.remove()
         songs = db.songs
         songs.insert_many(songsList)
-
-        #res=list(db.test.find({}))
-        #db.rates.remove()
-        #db.songs.remove()
-        #songs = db.songs
-
-        #songs.insert_many(songsList)
-        #collections_names=db.collection_names()
         return "db_erased"
 api.add_resource(ResetServer, '/reset')
--- flaskapp.py
+++ flaskapp.py
@@ -5,6 +5,9 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 api=Api(app)
+client = MongoClient("127.5.34.130", 27017)
+db = client.recmusicapiopenshift
+db.authenticate("admin","FeLZebfCiq_x")
 
 from api import general_api
 
