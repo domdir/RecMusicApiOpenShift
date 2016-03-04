ad3e98609fdd535a13ad3347bb2327ab3d170b8c
gianluca chiap <gianlucachiap@gmail.com>
2016-03-04 09:41:39 +0100

    test

--- flaskapp.py
+++ flaskapp.py
@@ -4,6 +4,8 @@ from flask_restful import Resource
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
 api=Api(app)
 client = MongoClient("127.5.34.130", 27017)
 db = client.recmusicapiopenshift
