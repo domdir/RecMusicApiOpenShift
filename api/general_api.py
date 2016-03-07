75fed4942543aa5b7b8c0296d599dd97bd006dd6
gianluca chiap <gianlucachiap@gmail.com>
2016-03-07 17:19:39 +0100

    test

--- api/general_api.py
+++ api/general_api.py
@@ -29,5 +29,5 @@ class ResetServer(Resource):
         app.logger.addHandler(logging.StreamHandler(sys.stdout))
         app.logger.setLevel(logging.ERROR)
 
-        return "db_erased"
+        return "db_erased_1"
 api.add_resource(ResetServer, '/reset')
