c2f65ca4c2ca3be0be440eae5d1bf8d774a6653f
gianluca chiap <gianlucachiap@gmail.com>
2016-04-11 11:02:57 +0200

    Added ping

--- core/__init__.py
+++ core/__init__.py
@@ -1,16 +1,24 @@
 print "1"
 from flask import Flask
-#import pandas as pd
-#import os
+# import pandas as pd
+import logging
+
+import os
+
+LOGFILE = "recmusic.log"
+
+logPath = os.path.join(os.environ["OPENSHIFT_LOG_DIR"], LOGFILE)
+logging.basicConfig(filename=logPath, level=logging.DEBUG)
+logging.debug('This message should go to the log file')
 
 print "2"
 mes_core = Flask(__name__)
-#app = Flask(__name__, static_folder='static', static_url_path='/static')
+# app = Flask(__name__, static_folder='static', static_url_path='/static')
 
-#movie_table = pd.read_csv(os.getcwd() + "/csv/movies_info.csv", ",")
-#movie_table = movie_table.drop("Unnamed: 0", 1)
+# movie_table = pd.read_csv(os.getcwd() + "/csv/movies_info.csv", ",")
+# movie_table = movie_table.drop("Unnamed: 0", 1)
 
 print "3"
 
 import routes
-#import database_manager
+# import database_manager
