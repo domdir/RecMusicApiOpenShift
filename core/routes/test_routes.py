b3fc6d18fb58161aceae5e4c319c04117537832b
gianluca chiap <gianlucachiap@gmail.com>
2016-04-11 17:09:06 +0200

    c

--- core/routes/test_routes.py
+++
@@ -1,9 +0,0 @@
-from core import mes_core
-
-from flask import request, jsonify, send_file
-
-@mes_core.route('/', methods=['GET'])
-def save_genres_liked():
-    print "save_genres_liked"
-    return jsonify({})
-
