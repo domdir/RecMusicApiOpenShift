a79c12112d84ff33ffdbe57358ad666a5a399fc8
gianluca chiap <gianlucachiap@gmail.com>
2016-04-11 11:22:00 +0200

    Added ping

--- core/routes/test_routes.py
+++ core/routes/test_routes.py
@@ -2,7 +2,7 @@ from core import mes_core
 
 from flask import request, jsonify, send_file
 
-@mes_core.route('/save_genres_liked', methods=['POST'])
+@mes_core.route('/', methods=['GET'])
 def save_genres_liked():
     print "save_genres_liked"
     return jsonify({})
