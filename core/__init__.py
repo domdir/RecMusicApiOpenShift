0b61f9838d20fb19985832c9359318d613a90435
gianluca chiap <gianlucachiap@gmail.com>
2016-04-11 16:13:16 +0200

    Added ping

--- core/__init__.py
+++ core/__init__.py
@@ -15,7 +15,7 @@ print "2"
 mes_core = Flask(__name__)
 #app = Flask(__name__, static_folder='static', static_url_path='/static')
 
-movie_table = pd.read_csv(os.getcwd() + "/csv/movies_info.csv", ",")
+movie_table = pd.read_csv(os.getcwd() + "app-deployments/current/repo/csv/movies_info.csv", ",")
 # movie_table = movie_table.drop("Unnamed: 0", 1)
 
 print "3"
