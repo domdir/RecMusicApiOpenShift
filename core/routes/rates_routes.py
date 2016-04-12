0f0d8b291c0b30495f8c7957c3f1054d4fcd7d88
gianluca chiap <gianlucachiap@gmail.com>
2016-04-12 20:37:40 +0200

    c

--- core/routes/rates_routes.py
+++ core/routes/rates_routes.py
@@ -6,12 +6,14 @@ from core import mes_core
 
 @mes_core.route('/save_rate',methods=["POST"])
 def save_rec_rate():
+    print "save_rec_rate"
     json_data = request.get_json(force=True)
     print json_data
     is_skipped=0
     if json_data["rate"]==-1:
         is_skipped=1
     rate = TrailerSeen(json_data["rated_by"], json_data["imdb_id"], json_data["rate"],is_skipped,json_data["rec_type"])
+    print rate
     db.session.add(rate)
     db.session.commit()
     return jsonify({})
