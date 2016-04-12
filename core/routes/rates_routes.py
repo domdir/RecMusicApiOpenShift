e7a630cc7ddf46d5c37f475122d91bb0cd9f96a3
gianluca chiap <gianlucachiap@gmail.com>
2016-04-12 20:28:43 +0200

    c

--- core/routes/rates_routes.py
+++ core/routes/rates_routes.py
@@ -12,7 +12,6 @@ def save_rec_rate():
     if json_data["rate"]==-1:
         is_skipped=1
     rate = TrailerSeen(json_data["rated_by"], json_data["imdb_id"], json_data["rate"],is_skipped,json_data["rec_type"])
-    print json_data["movie_id"]
     db.session.add(rate)
     db.session.commit()
     return jsonify({})
