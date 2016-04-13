a25382bbe932b6a075a659b3b07a4771cd512059
gianluca chiap <gianlucachiap@gmail.com>
2016-04-13 19:23:49 +0200

    c

--- core/routes/movies_routes.py
+++ core/routes/movies_routes.py
@@ -182,6 +182,7 @@ def get_movies():
     for i, r in movies_selected.iterrows():
         if r["IMDB_ID"] in rated_by_user_imdbid:
             r["IS_ALREADY_VOTED"]=True
+            print "already voted"
         else:
             r["IS_ALREADY_VOTED"]=False
         m_j = r.to_json()
