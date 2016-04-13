90d45188f3af14464655d819c1237b09c38b5b71
gianluca chiap <gianlucachiap@gmail.com>
2016-04-13 23:16:15 +0200

    c

--- core/routes/rec_routes.py
+++ core/routes/rec_routes.py
@@ -25,7 +25,7 @@ def get_rec():
 
     resp = {}
     for req in rec_request_list:
-        rec_type = rec_types.get(type, "RANDOM")
+        rec_type = rec_types.get(req, "RANDOM")
         table_to_use = get_table('all_table')()
         t = rec_router.get(rec_type)(table_to_use, 1)
         resp.update(t)
