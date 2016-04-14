5b4ec33824ceefa80e0a0ff2aa0615649e878cce
gianluca chiap <gianlucachiap@gmail.com>
2016-04-14 18:40:01 +0200

    c

--- core/database_manager/user_favorite_genres.py
+++ core/database_manager/user_favorite_genres.py
@@ -1,27 +1,5 @@
 from core.database_manager import db
 
-
-class UserFavoriteGenre(db.Model):
-    __tablename__ = 'user_favorite_genre'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(50))
-    fav_genre = db.Column(db.String(20))
-    genre_1 = db.Column(db.String(20))
-    genre_2 = db.Column(db.String(20))
-    genre_3 = db.Column(db.String(20))
-
-    def __init__(self, rated_by, genre1, genre2, genre3,genre4):
-        self.user_id = rated_by
-        self.fav_genre = genre1
-        self.genre_1 = genre1
-        self.genre_2 = genre2
-        self.genre_3 = genre3
-
-    def __repr__(self):
-        return 'user_id: {0} , genre1: {1}'.format(self.user_id, self.genre_1)
-
-
-"""
 class UserFavoriteGenre(db.Model):
     __tablename__ = 'user_favorite_genre'
     id = db.Column(db.Integer, primary_key=True)
@@ -41,4 +19,3 @@ class UserFavoriteGenre(db.Model):
     def __repr__(self):
         return 'user_id: {0} , genre1: {1}, genre2: {2} genre3: {3}, genre4: {4}' \
             .format(self.user_id, self.genre_1, self.genre_2, self.genre_3,self.genre_4)
-"""
--- core/routes/gernes_routes.py
+++ core/routes/gernes_routes.py
@@ -5,24 +5,20 @@ from core.database_manager import UserFavoriteGenre
 from core.database_manager import db
 
 
-@mes_core.route('/save_fav_genre', methods=['POST'])
-def save_fav_genre():
-    print "save_genres_liked"
-    json_data = request.get_json(force=True)
-    print json_data
-    genres = UserFavoriteGenre(json_data["user_id"], json_data["fav_genre"],None,None,None)
-    db.session.add(genres)
-    db.session.commit()
-    return jsonify({})
-
-
 @mes_core.route('/save_genres_liked', methods=['POST'])
 def save_genres_liked():
     print "save_genres_liked"
     json_data = request.get_json(force=True)
     print json_data
-    genres = UserFavoriteGenre(json_data["user_id"], json_data["genres_liked"],
-                               json_data["genres_liked"], json_data["genres_liked"], json_data["genres_liked"])
+    print json_data["genres_liked"][0]
+    print json_data["genres_liked"][1]
+    print json_data["genres_liked"][2]
+    print json_data["genres_liked"][3]
+
+
+
+    genres = UserFavoriteGenre(json_data["user_id"], json_data["genres_liked"][0],
+                               json_data["genres_liked"][1], json_data["genres_liked"][2], json_data["genres_liked"][3])
     db.session.add(genres)
     db.session.commit()
     return jsonify({})
