eee86ef3cb0e2083f13129c72b9d613b1ea81349
gianluca chiap <gianlucachiap@gmail.com>
2016-04-14 11:12:46 +0200

    c

--- core/rec_engine/random_rec.py
+++ core/rec_engine/random_rec.py
@@ -2,19 +2,17 @@ import random
 from core import get_table
 
 
-def random_rec(table_to_use, num_of_rec):
+def random_rec(table_to_use):
     print "random rec"
     movies_to_rec = {}
     tmp = []
     safe_iter = 0
 
-    while (len(movies_to_rec) < num_of_rec) and (safe_iter < 20):
+    while (len(movies_to_rec) < 1) and (safe_iter < 20):
         j = random.randrange(1, len(table_to_use.index))
         safe_iter += 1
         if j not in tmp:
             movie = table_to_use.iloc[j]
             movie["REC_TYPE"]="RANDOM"
             movie = movie.to_json()
-            movies_to_rec.update({len(movies_to_rec): movie})
-            tmp.append(j)
-    return movies_to_rec
+    return movie
