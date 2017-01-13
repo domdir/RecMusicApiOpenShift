76173e8481a6c1cb7eca35baa32b04028822c286
Stefano Cereda <cereda.ste@gmail.com>
2017-01-13 17:07:40 +0100

    reduced the number of questions

--- core/database_manager/questionnaire.py
+++ core/database_manager/questionnaire.py
@@ -20,14 +20,6 @@ class Questionnaire(db.Model):
     question_13 = db.Column(db.String(10))
     question_14 = db.Column(db.String(10))
     question_15 = db.Column(db.String(10))
-    question_16 = db.Column(db.String(10))
-    question_17 = db.Column(db.String(10))
-    question_18 = db.Column(db.String(10))
-    question_19 = db.Column(db.String(10))
-    question_20 = db.Column(db.String(10))
-    question_21 = db.Column(db.String(10))
-    question_22 = db.Column(db.String(10))
-    question_23 = db.Column(db.String(10))
 
     def __init__(self, rated_by,
                  question_1,
@@ -44,15 +36,7 @@ class Questionnaire(db.Model):
                  question_12,
                  question_13,
                  question_14,
-                 question_15,
-                 question_16,
-                 question_17,
-                 question_18,
-                 question_19,
-                 question_20,
-                 question_21,
-                 question_22,
-                 question_23,):
+                 question_15,):
 
         self.user_id = rated_by
         self.question_1 = question_1
@@ -70,11 +54,3 @@ class Questionnaire(db.Model):
         self.question_13 = question_13
         self.question_14 = question_14
         self.question_15 = question_15
-        self.question_16 = question_16
-        self.question_17 = question_17
-        self.question_18 = question_18
-        self.question_19 = question_19
-        self.question_20 = question_20
-        self.question_21 = question_21
-        self.question_22 = question_22
-        self.question_23 = question_23
