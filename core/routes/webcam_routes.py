baaec4e66ad6b1978aea8a865b7e30dda8496de9
Davide Anghileri <anghileridavide@mail.com>
2017-04-11 00:44:29 +0200

    webcam works with some limitations

--- core/routes/webcam_routes.py
+++ core/routes/webcam_routes.py
@@ -6,6 +6,7 @@ import cv2
 import base64
 import os 
 import pandas as pd
+from core import mes_core, get_table, get_table_by_genre
 
 @mes_core.route('/get_json_img', methods=['POST'])
 def get_json_img():
@@ -27,11 +28,20 @@ def get_json_img():
     # Starting from the obtained ID we get the imdb one.
     df = pd.read_csv(os.getcwd() + "/csv/movies_info.csv")
     # print df.columns.values
-
+    col_to_keep = ["IMDB_ID", "TITLE", "GENRES", "YEAR",
+               "LENGTH", "POSTER", "YOU_TUBE_ID", "IMDB_RATING", "IMDB_VOTES",
+               "f1", "f2", "f4", "f6"]
     # print("Init translation: "+ json_data) 
+    
+    path = os.getcwd() + "/csv/movie_info_reduced_with_genre.csv"
+    movie_table_all = pd.read_csv(path, ",")  # dtype=object)
+    movie_table_all.drop("Unnamed: 0", 1, inplace=True)
+    movieLineServer = movie_table_all[movie_table_all["TITLE"].str.lower() == json_data[0]["name"].lower()][col_to_keep]
+    movieIMDB = movieLineServer["IMDB_ID"].values[0]
+    json_data.append({'IMDB_ID':movieIMDB})
 
     print ("json data: ")  
     print (json_data)
-    print ("jsonify(json_data): ")
-    print jsonify(json_data)
+    # print ("jsonify(json_data): ")
+    # print jsonify(json_data)
     return jsonify(json_data)
