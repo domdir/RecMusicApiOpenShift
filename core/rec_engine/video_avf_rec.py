da30cdf9e844b6426d35ad98311e1e4dd1bdb3a9
stefanocereda <stefano1.cereda@mail.polimi.it>
2017-12-09 11:47:00 +0100

    missing files

---
+++ core/rec_engine/video_avf_rec.py
@@ -0,0 +1,73 @@
+import random
+from core import video_avf_sim
+from core.database_manager import trailer_seen
+from core import get_table
+from pandas import Series
+import numpy
+
+
+def video_avf_rec(user_id,num_of_rec):
+    rated_by_user = trailer_seen.TrailerSeen.query.filter_by(seen_by=user_id, is_skipped=0)
+    movies_seen=trailer_seen.TrailerSeen.query.filter_by(seen_by=user_id)
+    rated_imdb = {}
+    movies_to_exclude = []
+    for r in rated_by_user:
+        rated_imdb.update({r.imdb_id: r.rate})
+    for r in movies_seen:
+        movies_to_exclude.append(r.imdb_id)
+
+    final_array = []
+    i = 0
+    for row in video_avf_sim.itertuples():
+
+        num = float(0)
+        den = float(0)
+        i += 1
+        neigh_splitted = row[3].split(",")
+        for j in neigh_splitted:
+            imdb_sim = j.split(":")
+            imdb = imdb_sim[0]
+            if rated_imdb.get(imdb, None):
+                num += float(rated_imdb.get(imdb)) * float(imdb_sim[1])
+                den += float(imdb_sim[1])
+        if den == 0:
+            final_v = float(0)
+        else:
+            final_v = float(num / den)
+
+        if not final_v:
+            final_v = float(0)
+
+        final_array.append((row[1], final_v, row[2]))
+
+    dtype = [('IMDB_ID', 'S10'), ('PREDICTED_VOTE', float), ('IMDB_VOTES', int)]
+
+    numpy_final = numpy.array(final_array, dtype=dtype)
+    numpy_final = numpy.sort(numpy_final, order=['PREDICTED_VOTE'])
+    numpy_final = numpy_final[::-1]
+
+    all_table = get_table("all_table")()
+    all_table = all_table[~all_table["IMDB_ID"].isin(Series(movies_to_exclude))]
+    all_table.reset_index(drop=True, inplace=True)
+
+    final = {}
+
+    safe_iter = 0
+
+    while (len(final) < num_of_rec) and (safe_iter < 20):
+        rec = numpy_final[safe_iter]
+
+        movie = all_table[all_table["IMDB_ID"] == rec[0]].copy()
+        if len(movie.index):
+            movie.reset_index(drop=True, inplace=True)
+            movie = movie.iloc[0]
+            movie["REC_TYPE"] = "AUDIO"
+            movie["PREDICTED_VOTE"]=rec[1]
+
+            z = movie.to_json()
+            safe_iter += 1
+            final.update({len(final): z})
+        else:
+            safe_iter += 1
+
+    return final
